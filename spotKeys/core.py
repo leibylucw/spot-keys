fa26371c364e7f3902674b585c62acf8b61d34e5
Luke Leiby <lucaswleiby@gmail.com>
2024-11-02 12:26:59 -0400

    Move keyboard handling in separate module

--- spotKeys/core.py
+++ spotKeys/core.py
@@ -1,38 +1,16 @@
 """Facilitates app logic."""
 
-import keyboard
-
-from spotKeys import controls, speech
-
-
-def registerKeyboardShortcuts() -> None:
-	"""
-	Registers keyboard shortcuts with the keyboard library.
-
-	Each shortcut is assigned a wrapper function that represents a high-level playback function
-
-	These are hard-coded for now.
-	"""
-
-	keyboard.add_hotkey('ctrl+win+alt+p', controls.playOrPause)
-	keyboard.add_hotkey('ctrl+win+alt+left', controls.previousTrack)
-	keyboard.add_hotkey('ctrl+win+alt+[', controls.rewind)
-	keyboard.add_hotkey('ctrl+win+alt+]', controls.fastForward)
-	keyboard.add_hotkey('ctrl+win+alt+right', controls.nextTrack)
-	keyboard.add_hotkey('ctrl+win+alt+down', controls.decreaseVolume)
-	keyboard.add_hotkey('ctrl+win+alt+up', controls.increaseVolume)
-	keyboard.add_hotkey('ctrl+win+alt+m', controls.muteOrUnmute)
-	keyboard.add_hotkey('ctrl+win+alt+t', controls.getTrackDescription)
+from spotKeys import controls, keyboardHandler, speech
 
 
 def initialize() -> None:
 	"""Initializes the core logic by initializing speech and registering keyboard shortcuts."""
 
 	speech.initialize()
-	registerKeyboardShortcuts()
+	keyboardHandler.registerKeyboardShortcuts()
 
 
 def run() -> None:
 	"""Awaits user input."""
 
-	keyboard.wait('ctrl+win+alt+q')
+	keyboardHandler.waitForInput()
---
+++ spotKeys/keyboardHandler.py
@@ -0,0 +1,39 @@
+"""Configures keyboard shortcut bindings."""
+
+import json
+
+import keyboard
+
+from spotKeys import controls
+
+DEFAULT_KEYBOARD_PREFIX = 'ctrl+alt+shift'
+DEFAULT_KEYBOARD_SHORTCUTS = {
+	'p': controls.playOrPause,
+	'left': controls.previousTrack,
+	'right': controls.nextTrack,
+	'down': controls.decreaseVolume,
+	'up': controls.increaseVolume,
+	'[': controls.rewind,
+	']': controls.fastForward,
+	'm': controls.muteOrUnmute,
+	't': controls.getTrackDescription,
+}
+
+
+def registerKeyboardShortcuts() -> None:
+	"""
+	Registers keyboard shortcuts with the keyboard library.
+
+	Each shortcut is assigned a wrapper function that represents a high-level playback function
+
+	It uses hard-coded defaults for now.
+	"""
+
+	for shortcut, control in DEFAULT_KEYBOARD_SHORTCUTS.items():
+		keyboard.add_hotkey(f'{DEFAULT_KEYBOARD_PREFIX}+{shortcut}', control)
+
+
+def waitForInput() -> None:
+	"""Blocks using the keyboard library's `wait` loop."""
+
+	keyboard.wait(f'{DEFAULT_KEYBOARD_PREFIX}+q')
