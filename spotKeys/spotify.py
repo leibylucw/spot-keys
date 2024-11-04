8428c5a3a70c75d714434e5bd8a11edd7273b06d
Luke Leiby <lucaswleiby@gmail.com>
2024-11-03 21:44:41 -0500

    Add a build process

--- spotKeys/spotify.py
+++ spotKeys/spotify.py
@@ -1,21 +1,28 @@
 """Sets up Spotify-related config."""
 
-import os
+import sys
+from pathlib import Path
 
-import environs
 import spotipy
+from environs import Env
 from spotipy.oauth2 import SpotifyOAuth
 
-# Read environment variables from `.env`
-environs.Env().read_env()
+env = Env()
+env.read_env(path=Path(__file__).resolve().parent / '.env')
 
+# Check if the application is frozen
+appPath = Path(sys._MEIPASS) if getattr(sys, 'frozen', False) else Path(__file__).resolve().parent
 
-# Initialize spotipy
+homeDirectory = Path.home()
+spotifyCachePath = homeDirectory / '.spotify_cache'
+
+# Initialize spotipy with environment variables
 SPOTIFY_HANDLER = spotipy.Spotify(
 	auth_manager=SpotifyOAuth(
-		client_id=os.getenv('CLIENT_ID'),
-		client_secret=os.getenv('CLIENT_SECRET'),
-		redirect_uri=os.getenv('REDIRECT_URI'),
-		scope=os.getenv('SCOPES'),
+		client_id=env('CLIENT_ID'),
+		client_secret=env('CLIENT_SECRET'),
+		redirect_uri=env('REDIRECT_URI'),
+		scope=env('SCOPES'),
+		cache_path=str(spotifyCachePath),
 	)
 )
