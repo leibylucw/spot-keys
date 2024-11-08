4f81eaca18366a5a33be177629efe97ee6880843
Luke Leiby <lucaswleiby@gmail.com>
2024-11-07 22:28:47 -0500

    Properly clean up resources and exit app with announcement

--- spotKeys/core.py
+++ spotKeys/core.py
@@ -7,6 +7,7 @@ def initialize() -> None:
 	"""Initializes the core logic by initializing speech and registering keyboard shortcuts."""
 
 	speech.initialize()
+	speech.say('Spot Keys is ready')
 	keyboardHandler.registerKeyboardShortcuts()
 
 
@@ -14,3 +15,14 @@ def run() -> None:
 	"""Awaits user input."""
 
 	keyboardHandler.waitForInput()
+
+
+def exit() -> None:
+	"""Cleans up resources and provides feedback that the application is exiting."""
+
+	speech.say('Exiting Spot Keys')
+
+	while speech.isSpeaking():
+		pass
+
+	speech.destroy()
