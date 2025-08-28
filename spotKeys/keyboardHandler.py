790e95d05522fa06240e1cdd9212dd7818a266f1
Luke Leiby <lucaswleiby@gmail.com>
2025-08-28 17:58:11 -0400

    Default to in-memory keyboard modifiers and shortcuts

--- spotKeys/keyboardHandler.py
+++ spotKeys/keyboardHandler.py
@@ -1,96 +1,40 @@
 """Configures keyboard shortcut bindings."""
 
-import json
+from typing import Callable, Dict
 
 import keyboard
 
 from spotKeys import controls
 
-# Default keyboard modifiers
 DEFAULT_KEYBOARD_MODIFIERS = 'alt+shift'
-
-# Default mapping of shortcuts to function names
-DEFAULT_KEYBOARD_SHORTCUTS = {
-	'p': 'playOrPause',
-	'left': 'previousTrack',
-	'right': 'nextTrack',
-	'down': 'decreaseVolume',
-	'up': 'increaseVolume',
-	'[': 'rewind',
-	']': 'fastForward',
-	'm': 'muteOrUnmute',
-	'n': 'getCurrentTrackName',
-	'r': 'getCurrentTrackArtistNames',
-	'a': 'getCurrentTrackAlbumName',
-	'i': 'getCurrentTrackDetails',
-	'l': 'likeCurrentTrack',
-	'd': 'dislikeCurrentTrack',
-	'u': 'copyCurrentTrackURL',
+QUIT_SHORTCUT = f'{DEFAULT_KEYBOARD_MODIFIERS}+q'
+DEFAULT_KEYBOARD_SHORTCUTS: Dict[str, Callable[[], None]] = {
+	'p': controls.playOrPause,
+	'left': controls.previousTrack,
+	'right': controls.nextTrack,
+	'down': controls.decreaseVolume,
+	'up': controls.increaseVolume,
+	'[': controls.rewind,
+	']': controls.fastForward,
+	'm': controls.muteOrUnmute,
+	'n': controls.getCurrentTrackName,
+	'r': controls.getCurrentTrackArtistNames,
+	'a': controls.getCurrentTrackAlbumName,
+	'i': controls.getCurrentTrackDetails,
+	'l': controls.likeCurrentTrack,
+	'd': controls.dislikeCurrentTrack,
+	'u': controls.copyCurrentTrackURL,
 }
-
-DEFAULT_QUIT_KEYBOARD_SHORTCUT = f'{DEFAULT_KEYBOARD_MODIFIERS}+q'
-
-# Maps function names to controls
-functionsToControls = {
-	'playOrPause': controls.playOrPause,
-	'previousTrack': controls.previousTrack,
-	'nextTrack': controls.nextTrack,
-	'decreaseVolume': controls.decreaseVolume,
-	'increaseVolume': controls.increaseVolume,
-	'rewind': controls.rewind,
-	'fastForward': controls.fastForward,
-	'muteOrUnmute': controls.muteOrUnmute,
-	'getCurrentTrackName': controls.getCurrentTrackName,
-	'getCurrentTrackArtistNames': controls.getCurrentTrackArtistNames,
-	'getCurrentTrackAlbumName': controls.getCurrentTrackAlbumName,
-	'getCurrentTrackDetails': controls.getCurrentTrackDetails,
-	'likeCurrentTrack': controls.likeCurrentTrack,
-	'dislikeCurrentTrack': controls.dislikeCurrentTrack,
-	'copyCurrentTrackURL': controls.copyCurrentTrackURL,
-}
-
-DEFAULT_JSON_CONFIG_PATH = 'data/config.json'
 
 
 def registerKeyboardShortcuts() -> None:
-	"""
-	Registers keyboard shortcuts with the keyboard library.
+	"""Register built-in keyboard shortcuts with the keyboard library."""
 
-	Each shortcut is assigned a wrapper function that represents a high-level playback function
-
-	It first attempts to load the default JSON config,
-	otherwise, it uses the hard-coded values in code.
-	"""
-
-	# Get keyboard shortcuts
-	# Not the right place, but it'll be fixed later
-	try:
-		with open(DEFAULT_JSON_CONFIG_PATH, 'r') as file:
-			config = json.loads(file.read())
-
-		keyboardModifiers = config['keyboardModifiers']
-		keyboardShortcuts = config['keyboardShortcuts']
-	except FileNotFoundError:
-		keyboardModifiers = DEFAULT_KEYBOARD_MODIFIERS
-		keyboardShortcuts = DEFAULT_KEYBOARD_SHORTCUTS
-
-	for shortcut, control in keyboardShortcuts.items():
-		keyboard.add_hotkey(f'{keyboardModifiers}+{shortcut}', functionsToControls[control])
+	for key, function in DEFAULT_KEYBOARD_SHORTCUTS.items():
+		keyboard.add_hotkey(f'{DEFAULT_KEYBOARD_MODIFIERS}+{key}', function)
 
 
 def waitForInput() -> None:
-	"""Blocks using the keyboard library's `wait` loop."""
+	"""Block until the quit shortcut is pressed."""
 
-	# Get keyboard shortcuts
-	# Not the right place, but it'll be fixed later
-	try:
-		with open(DEFAULT_JSON_CONFIG_PATH, 'r') as file:
-			config = json.loads(file.read())
-
-		keyboardModifiers = config['keyboardModifiers']
-		quitKeyboardShortcut = f'{keyboardModifiers}+q'
-	except FileNotFoundError:
-		keyboardModifiers = DEFAULT_KEYBOARD_MODIFIERS
-		quitKeyboardShortcut = DEFAULT_QUIT_KEYBOARD_SHORTCUT
-
-	keyboard.wait(quitKeyboardShortcut)
+	keyboard.wait(QUIT_SHORTCUT)
